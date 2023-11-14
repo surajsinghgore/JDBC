727da112dd7898c76a54b8784afc73c2cf09516d
surajsinghgore <surajthakurrs45@gmail.com>
2023-11-14 19:57:29 +0530

    table structure update

--- mysql/UpdateTableStructure.java
+++ mysql/UpdateTableStructure.java
@@ -1,8 +1,8 @@
+// #6
 package mysql;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.Statement;
 
 public class UpdateTableStructure {
